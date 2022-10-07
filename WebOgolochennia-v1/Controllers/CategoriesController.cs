94e76cb9eabb033ea7a8e4056ca5241eb00a5281
Ig2525 <ig25@ukr.net>
2022-10-07 21:28:30 +0300

    Перша проба

---
+++ WebOgolochennia-v1/Controllers/CategoriesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Xml.Linq;
+using WebOgolochennia_v1.Data;
+using WebOgolochennia_v1.Data.Entities;
+using WebOgolochennia_v1.Models;
+
+namespace WebOgolochennia_v1.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        //Вивід списку з БД
+        private readonly MyAppContext _appContext;
+        public CategoriesController(MyAppContext appContext)
+        {
+            _appContext = appContext;
+        }
+        // Відображення списку категорій чи оголошень
+        public IActionResult Index()
+        {
+            var list = _appContext.Categories.Select(x => new CategoryItemViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Advertisement = x.Advertisement,
+                Price = x.Price,
+                Tel1 = x.Tel1,
+                Tel2 = x.Tel2,
+                Other = x.Other,
+                PublicInNewspaper = x.PublicInNewspaper,
+                InicialData = x.InicialData,
+                FinalData = x.FinalData,
+                Highlighted = x.Highlighted,
+                Frame = x.Frame,
+                Background = x.Background,
+                PublicInSite = x.PublicInSite,
+                InitialWeek = x.InitialWeek,
+                Up = x.Up,
+                Paid = x.Paid,
+            }).ToList();
+            return View();
+        }
+        //Показуємо сторінку створення категорії
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Зберігаємо категорію в БД
+        [HttpPost]
+        public IActionResult Create(CategoryCreateViewModels model)
+        {
+            CategoryEntity entity = new CategoryEntity()
+            {
+                Name = model.Name,
+                Advertisement = model.Advertisement,
+                Price = model.Price,
+                Tel1 = model.Tel1,
+                Tel2 = model.Tel2,
+                Other = model.Other,
+                PublicInNewspaper = model.PublicInNewspaper,
+                InicialData = model.InicialData,
+                FinalData = model.FinalData,
+                Highlighted = model.Highlighted,
+                Frame = model.Frame,
+                Background = model.Background,
+                PublicInSite = model.PublicInSite,
+                InitialWeek = model.InitialWeek,
+                Up = model.Up,
+                Paid = model.Paid,
+            };
+            _appContext.Categories.Add(entity);
+            _appContext.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ WebOgolochennia-v1/Models/CategoryViewModels.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebOgolochennia_v1.Models
+{
+    //Ввід даних
+    public class CategoryCreateViewModels
+    {
+        [Display(Name="Рубрика")]
+        public string Name { get; set; }
+        public string Advertisement { get; set; }
+        public double Price { get; set; }
+        public string Tel1 { get; set; }
+        public string Tel2 { get; set; }
+        public string Other { get; set; }
+        public bool PublicInNewspaper { get; set; }
+        public string InicialData { get; set; }
+        public string FinalData { get; set; }
+        public bool Highlighted { get; set; }
+        public bool Frame { get; set; }
+        public bool Background { get; set; }
+        public bool PublicInSite { get; set; }
+        public int InitialWeek { get; set; }
+        public bool Up { get; set; }
+        public bool Paid { get; set; }
+    }
+    //Відображення даних
+    public class CategoryItemViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name ="Рубрика")]
+        public string Name { get; set; }
+        public string Advertisement { get; set; }
+        public double Price { get; set; }
+        public string Tel1 { get; set; }
+        public string Tel2 { get; set; }
+        public string Other { get; set; }
+        public bool PublicInNewspaper { get; set; }
+        public string InicialData { get; set; }
+        public string FinalData { get; set; }
+        public bool Highlighted { get; set; }
+        public bool Frame { get; set; }
+        public bool Background { get; set; }
+        public bool PublicInSite { get; set; }
+        public int InitialWeek { get; set; }
+        public bool Up { get; set; }
+        public bool Paid { get; set; }
+    }
+
+}
